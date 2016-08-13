f3d59a5a6c1bddd5bf87125f91975d7297783938
Brannen Hall <Brannen@HurtadoHall.me>
2016-08-13 14:48:00 -0500

    Added some PHP, testing if github.io has php support

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+  <footer>
+    <p>This website is currently a work in progress</p>
+    <p>Hosted on <a href="github.io">github.io</a>
+  </footer>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Brannen Hall</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <header>
+  <h1>Brannen's Portfolio</h1>
+  <table>
+    <tbody>
+      <tr>
+        <td><a href="program.html">Programming</a></td>
+        <td><a href="speech.html">Speech—Langauge—Hearing Sciences</a></td>
+      </tr>
+    </tbody>
+  </table>
+</header>

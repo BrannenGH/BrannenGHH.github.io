059b9350a9eb18d087aacfd7298c090af992c5d6
BrannenGHall <Brannen@HurtadoHall.me>
2016-10-28 14:28:49 -0500

    Removed old website

--- footer.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-  <footer>
-    <p>This website is currently a work in progress</p>
-    <p>Hosted on <a href="github.io">github.io</a>
-  </footer>
-</html>
--- speech.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<?php require("header.php");>
-  <body>
-  <p> In progress </p>
-  </body>
-<?php require("footer.php");>
